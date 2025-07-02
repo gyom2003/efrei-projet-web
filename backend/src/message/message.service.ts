33a1df314f549c7a29b6aca07a7d3d2cb37ed09f
Rayane Besrour <rayanebesrour3@gmail.com>
2025-07-02 20:29:40 +0200

    Update message.service.ts

--- backend/src/message/message.service.ts
+++ backend/src/message/message.service.ts
@@ -3,9 +3,6 @@ import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class MessageService {
-  saveMessage(mockMessage: { id: string; conversationId: string; authorId: string; content: string; timestamp: number; }) {
-    throw new Error('Method not implemented.');
-  }
   constructor(private prisma: PrismaService) {}
 
   findByConversation(conversationId: string) {
