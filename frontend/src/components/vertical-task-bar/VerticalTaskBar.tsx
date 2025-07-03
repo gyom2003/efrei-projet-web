80a0c10d033c81f9701641b834fa3f4916ff54e3
Arthur Manceau <102350268+armanceau@users.noreply.github.com>
2025-07-03 14:17:37 +0200

    feat(): logout + refacto style

--- frontend/src/components/vertical-task-bar/VerticalTaskBar.tsx
+++ frontend/src/components/vertical-task-bar/VerticalTaskBar.tsx
@@ -1,26 +1,36 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Chat, Person, Gear, BoxArrowLeft } from "react-bootstrap-icons";
 import styles from "./verticalTaskBar.module.css";
+import { useApolloClient } from "@apollo/client";
 
 export default function VerticalTaskBar() {
-  const [selected, setSelected] = useState<number | null>(null);
+  const [selected, setSelected] = useState<number>(0);
+  const navigate = useNavigate();
+  const client = useApolloClient();
+
+  const handleLogout = async () => {
+    localStorage.removeItem("token");
+    await client.clearStore();
+    navigate("/login");
+  };
 
   const icons = [
-    { id: 0, component: <Chat size={20} /> },
-    { id: 1, component: <Person size={20} /> },
-    { id: 2, component: <Gear size={20} /> },
-    { id: 3, component: <BoxArrowLeft size={20} /> },
+    { id: 0, component: <Chat size={20} />, onClick: () => setSelected(0) },
+    { id: 1, component: <Person size={20} />, onClick: () => setSelected(1) },
+    { id: 2, component: <Gear size={20} />, onClick: () => setSelected(2) },
+    { id: 3, component: <BoxArrowLeft size={20} />, onClick: handleLogout },
   ];
 
   return (
     <section>
-      {icons.map(({ id, component }) => (
+      {icons.map(({ id, component, onClick }) => (
         <div
           key={id}
           className={`${styles["section-icon"]} ${
             selected === id ? styles.selected : ""
-          }`}
-          onClick={() => setSelected(id)}
+          } ${id === 3 ? styles.logout : ""}`}
+          onClick={onClick}
         >
           {component}
         </div>
